3fa99e456f3f3cce74924c46b36ec90760c0a49b
Matheus Pinheiro <pinheiropassosm@gmail.com>
2023-11-12 20:15:57 -0300

    Added main components.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Container from './components/container'
+import CurrentWeather from './components/currentWeather'
+
+function App() {
+
+  return (
+    <>
+      <Container>
+        <CurrentWeather />
+      </Container >
+    </>
+  )
+}
+
+export default App
